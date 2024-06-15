d111a2e40314d97e796cd6e716016c19166b8ad2
IslamAbukoush <islam.abu.koush@isa.utm.md>
2024-06-15 23:40:19 +0300

    gg

--- Front End/JavaScript/preferences.js
+++ Front End/JavaScript/preferences.js
@@ -1,3 +1,5 @@
+let IP = "http://192.168.93.133:5000"
+
 let prefsCont = document.querySelector(".preferencesContainer");
 let preferences = document.getElementById('preferences');
 preferences.replaceChildren();
@@ -50,7 +52,7 @@ function updateWords(array) {
 
 async function sendRequest(input) {
     product = input;
-    const response = await fetch('http://192.168.43.133:5000/prefs', {
+    const response = await fetch(IP+'/prefs', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -66,7 +68,7 @@ async function sendRequest(input) {
 }
 
 async function getArticles(input) {
-    const response = await fetch('http://192.168.43.133:5000/recs', {
+    const response = await fetch(IP+'/recs', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -80,8 +82,11 @@ async function getArticles(input) {
         console.log(`Error: ${data.error}`)
     }
 
-    for key, value in data['result'].items():
-        print(key, value)
+    for (const [key, value] of Object.entries(data['result'])) {
+        for(link of value) {
+            addCard(key, link);
+        }
+    }
 
 }
 
