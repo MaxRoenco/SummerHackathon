04e788697c71dccf20e6ce5e95db0386f75f3a05
Roenco Maxim <maxroenco2004@gmail.com>
2024-06-15 19:08:27 +0300

    gfghlkjlklklnkjh

---
+++ Front End/JavaScript/interests.js
@@ -0,0 +1,35 @@
+const modal = document.getElementById("interestsModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+
+
+let selectedInterests = [];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+const interestItems = document.querySelectorAll('.interest-item');
+
+interestItems.forEach(item => {
+    item.onclick = function () {
+        item.classList.toggle('selected');
+        const interest = item.textContent;
+        if (item.classList.contains('selected')) {
+            selectedInterests.push(interest);
+        } else {
+            selectedInterests = selectedInterests.filter(i => i !== interest);
+        }
+        console.log(selectedInterests);
+    }
+});
