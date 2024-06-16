53026b5faac09c9d2258224e9f2d60398debe62b
Roenco Maxim <maxroenco2004@gmail.com>
2024-06-16 05:38:51 +0300

    jkksdflja

--- Front End/JavaScript/link_search.js
+++ Front End/JavaScript/link_search.js
@@ -1,7 +1,14 @@
 let IP = "http://192.168.43.133:5000"
 let input = document.querySelector("#inputlink");
 let btn = document.querySelector(".linkButton");
-let out = document.querySelector("#output");
+let pros = document.querySelector('.pros');
+let cons = document.querySelector('.cons');
+let conclusionOutput = document.querySelector('.conclusionOutput');
+let titleOutput = document.querySelector('.titleOutput');
+let description = document.querySelector('.description');
+let outputContainer = document.querySelector('.outputContainer');
+let prosP = document.querySelector('#prosP');
+let consP = document.querySelector('#consP');
 
 btn.addEventListener("click", _ => {
     console.log("sup");
@@ -9,7 +16,7 @@ btn.addEventListener("click", _ => {
 })
 
 async function getComp(url) {
-    const response = await fetch(IP+'/comp', {
+    const response = await fetch(IP + '/comp', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -18,9 +25,26 @@ async function getComp(url) {
     });
     const data = await response.json();
     if (response.ok) {
+        pros.replaceChildren();
+        cons.replaceChildren();
+        pros.append(prosP);
+        cons.append(consP);
         console.log(data)
-        out.textContent = data["result"];
         obj = JSON.parse(data["result"]);
+        titleOutput.textContent = obj.title;
+        conclusionOutput.textContent = obj.conclusion;
+        description.textContent = obj.description;
+        obj['cons'].forEach(element => {
+            let li = document.createElement('li');
+            li.textContent = element
+            cons.append(li);
+        });
+        obj['pros'].forEach(element => {
+            let li = document.createElement('li');
+            li.textContent = element
+            pros.append(li);
+        });
+        outputContainer.style.display = 'grid';
     } else {
         console.log(`Error: ${data.error}`)
     }
